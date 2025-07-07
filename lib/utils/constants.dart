7d352b15d54a6c7c97e40a7e6ca8f07a406579fa
hridoycse197 <hridoycse.eng@gmail.com>
2025-07-08 03:24:29 +0600

    sidebar

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -18,3 +18,17 @@ class Onboarding {
   static String get kBoard2 =>
       'assets/images/onBoarding/onboarding_date_icon.png';
 }
+
+class SideBar {
+  static String get homeIcon => 'assets/images/side_bar/home_icon.png';
+  static String get customerServiceIcon =>
+      'assets/images/side_bar/customer_service_list_icon.png';
+  static String get dispatchingIcon =>
+      'assets/images/side_bar/dispatch_calendar_icon.png';
+  static String get formIcon => 'assets/images/side_bar/form_icon.png';
+  static String get settingsIcon => 'assets/images/side_bar/settings_icon.png';
+  static String get logoutIcon => 'assets/images/side_bar/logout_icon.png';
+  static String get itemsIcon => 'assets/images/side_bar/items_icon.png';
+  static String get profileGoIcon =>
+      'assets/images/side_bar/profile_go_icon.png';
+}
